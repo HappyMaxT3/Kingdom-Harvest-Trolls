90816f1897d60a668a64fc28beb671698c5c64af
HappyMaxT3 <m9775531181@gmail.com>
2024-08-19 16:10:12 +0300

    added pause menu

---
+++ Kingdom-Harvest-Trolls/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public static bool GameIsPause = false;
+        public GameObject PauseMenuUI;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (GameIsPause)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            Debug.Log("Continue clicked");
+            PauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPause = false;
+        }
+
+        public void Pause()
+        {
+            PauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPause = true;
+        }
+
+        public void LoadMenu()
+        {
+            Debug.Log("loaded menu");
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+}
+
+
