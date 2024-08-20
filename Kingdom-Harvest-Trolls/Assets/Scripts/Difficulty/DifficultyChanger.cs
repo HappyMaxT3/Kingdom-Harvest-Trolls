aace3e1bdfe8447a21b9199be730d65b707b6df8
Jenny Akhtiamova <zhanna.axt@bk.ru>
2024-08-20 17:21:07 +0300

    CALL ME DADDY

--- Kingdom-Harvest-Trolls/Assets/Scripts/Difficulty/DifficultyChanger.cs
+++ Kingdom-Harvest-Trolls/Assets/Scripts/Difficulty/DifficultyChanger.cs
@@ -28,6 +28,7 @@ namespace Game.UI
         {
             DifficultyManager.Instance.SetDifficulty(_difficulties[_difficultyDropdown.value]);
             SaveLoadSystem.SaveDifficulty(_difficultyDropdown.value);
+            Debug.Log($"{_difficultyDropdown.value}");
         }
     }
 }
