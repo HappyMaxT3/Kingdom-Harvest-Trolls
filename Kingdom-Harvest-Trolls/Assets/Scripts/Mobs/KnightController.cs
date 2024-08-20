de1ac7aad0ef92777a14387bd4c3ac15b38f8468
Jenny Akhtiamova <zhanna.axt@bk.ru>
2024-08-20 08:06:22 +0300

    CUMmit

--- Kingdom-Harvest-Trolls/Assets/Scripts/Mobs/KnightController.cs
+++ Kingdom-Harvest-Trolls/Assets/Scripts/Mobs/KnightController.cs
@@ -1,18 +1,48 @@
+using Game;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KnightController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject target;
+
+    public int max_health = 50;
+    private int current_health;
+    public int attack = 5;
+    public float speed = 20;
+
+    private void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Troll");
+        if (target == null)
+        {
+            DestroyKnight();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (target == null)
+            return;
+
+        // ¬ычисл€ем направление движени€
+        Vector3 direction = target.transform.position - transform.position;
+
+        // Ќормализуем вектор направлени€, чтобы получить единичный вектор
+        direction.Normalize();
+
+        // ѕеремещаем моба
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void DestroyKnight()
+    {
+        Destroy(gameObject, 0f);
     }
 }
